61f44c4f1ca9297f1b87e8231117b536bb0d98b5
Michael Kohler <mkohler@picobudget.com>
2013-11-02 11:02:31 +0100

    License header and authors added

--- src/com/example/bewegungsmeldermain/MainActivity.java
+++ src/com/example/bewegungsmeldermain/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.bewegungsmeldermain;
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Author: Ralf Wittich <ralf.wittich@gmx.ch>
+ * Contributors:
+ * 	- your name <your@mail.com> */
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
--- src/com/example/bewegungsmeldermain/UserSettingActivity.java
+++ src/com/example/bewegungsmeldermain/UserSettingActivity.java
@@ -1,5 +1,13 @@
 package com.example.bewegungsmeldermain;
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Author: Michael Kohler <mkohler@picobudget.com>
+ * Contributors:
+ * 	- your name <your@mail.com> */
+
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
