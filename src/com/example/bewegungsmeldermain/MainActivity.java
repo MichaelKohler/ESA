2d5cc8919cda7bad2c232108b45d5875189bf3a4
i3ullit <bullit@gmx.ch>
2013-11-02 12:46:04 +0100

    Changed e-mail

--- src/com/example/bewegungsmeldermain/MainActivity.java
+++ src/com/example/bewegungsmeldermain/MainActivity.java
@@ -4,7 +4,7 @@ package com.example.bewegungsmeldermain;
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * 
- * Author: Ralf Wittich <ralf.wittich@gmx.ch>
+ * Author: Ralf Wittich <bullit@gmx.ch>
  * Contributors:
  * 	- Ralf Wittich <bullit@gmx.ch> */
 
