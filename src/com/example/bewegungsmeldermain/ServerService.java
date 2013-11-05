d4b3e405c496aec78e11b4dfad9f9d93812c1fdd
i3ullit <bullit@gmx.ch>
2013-11-05 23:23:25 +0100

    Codegerüst Service Klassen erstellt, im Manifest registriert.

---
+++ src/com/example/bewegungsmeldermain/ServerService.java
@@ -0,0 +1,51 @@
+package com.example.bewegungsmeldermain;
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Author: Ralf Wittich <bullit@gmx.ch>
+ * Contributors:
+ * 	- Ralf Wittich <bullit@gmx.ch> 
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ServerService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onCreate()
+	 */
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onDestroy()
+	 */
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onStart(android.content.Intent, int)
+	 */
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
+
+}
