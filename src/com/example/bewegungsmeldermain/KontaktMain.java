174862d5c5e2dd747afb4bca13eec0aae5e922a0
i3ullit <bullit@gmx.ch>
2013-11-18 21:57:24 +0100

    Lizenz / Author eingefuegt

--- src/com/example/bewegungsmeldermain/KontaktMain.java
+++ src/com/example/bewegungsmeldermain/KontaktMain.java
@@ -1,5 +1,16 @@
 package com.example.bewegungsmeldermain;
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Author: Mario Aloise <aloise.mario@gmail.com> 
+ * Contributors:
+ * 	- Ralf Wittich <bullit@gmx.ch> 
+ *  - Mario Aloise <aloise.mario@gmail.com> 
+ *  - Michael Kohler <mkohler@picobudget.com>
+ *  */
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
