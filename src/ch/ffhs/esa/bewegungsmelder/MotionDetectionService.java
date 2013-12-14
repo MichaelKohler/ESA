ead1218dcda8e8b73711bd755c31391fc44bfd84
i3ullit <bullit@gmx.ch>
2013-12-14 16:34:27 +0100

    Motion Detection update: Start / Stop des Service Broadcasting vorbereitet (Ausgabe auf LogCat atm) Timer stoppt sich selbst wenn abgelaufen Timer wird bei Bewegung neu gestartet Offen: Timer wird auch nach Ablauf neu gestartet Offen: Paremtrierung ist noch hardcoded. Sollte in die Settings.

--- src/ch/ffhs/esa/bewegungsmelder/MotionDetectionService.java
+++ src/ch/ffhs/esa/bewegungsmelder/MotionDetectionService.java
@@ -23,44 +23,58 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MotionDetectionService extends Service implements SensorEventListener{
-	public static final String MOTION_DETECTION_ACTION = "ACTIVITY_HIER_EINTRAGEN";	// TODO: Activity eintragen
+	public static final String MOTION_DETECTION_ACTION = "ch.ffhs.esa.bewegungsmelder.MOTION_DETECTION_ACTION";
 	private static final String TAG = MotionDetectionService.class.getSimpleName();
 	
 	 private SensorManager mSensorManager;
      private Sensor mAccelerometer;
      private Timer motionTimer = null;
      private BroadcastMotionDetectionStatus sendStatus = null;
-     private int delayTime = 300000; //in ms (300000 == 5 min)	//TODO: delayTime auslesen
-     private int refreshTime = 5000; // in ms
+     private int delayTime = 10000; //in ms (300000 == 5 min)	//TODO: delayTime auslesen
+     private int refreshTime = 1000; // in ms
      
      
 	// Inner Class: Definition des Broadcasters
 	private class BroadcastMotionDetectionStatus extends TimerTask{
 		
 		int counter = 0;
-		boolean timerRunning;
+		boolean timerRunning = false;
 		@Override
 		public void run() {	
-		//	Intent intent = new Intent(MOTION_DETECTION_ACTION);		
+			Intent i = new Intent(MOTION_DETECTION_ACTION);
+					
 			int timer = delayTime - counter * refreshTime;
-			
-			Log.d(TAG, "Broadcasting! timer: " + timer + "ms to go"  );
+			counter++;
+		
+			Log.d(TAG, "Broadcasting! timer: " + timer + "ms to go" + " counter: " +counter + " refreshTime: " +refreshTime  );
 			
 			// stop if timer == 0;
-			if(timer != 0){
+			String toastMsg;
+			
+			if(timer > 0){
 				timerRunning = true;
+				toastMsg = "running";
 			}
-			else
-			{
+			else {
 				timerRunning= false;
+				toastMsg = "stopped";
 			};
 			// TODO Rest des Timer Broadcasters
-			//intent.putExtra("TIMER_RUNNING", timerRunning);
-			//sendBroadcast(intent);
+			i.putExtra("TIMER_RUNNING", timerRunning);
+			Log.d(TAG, "Broadcast! Timer: " + timer /1000 + "s until alarm. Timer Status: " + toastMsg);
+			sendBroadcast(i);	 
+			
+			if(!timerRunning){
+			this.cancel();
+			Log.d(TAG, "timer stopped");
+			};
+		}	
+		public boolean isRunning(){
+			return timerRunning;
 		}
-		
 	}
 	
 	 // Wird fuer IPC benoetigt (inter-process communication")
@@ -84,6 +98,10 @@ public class MotionDetectionService extends Service implements SensorEventListen
 	public void onDestroy() {
 		super.onDestroy();
 		mSensorManager.unregisterListener(this);
+		
+		sendStatus.cancel();
+		motionTimer.cancel();
+		motionTimer.purge();
 		Log.d(TAG, "destroyed");	
 	}
 
@@ -92,41 +110,32 @@ public class MotionDetectionService extends Service implements SensorEventListen
 	 */
 	@Override
 	public void onStart(Intent intent, int startId) {
-		// TODO Auto-generated method stub
 		super.onStart(intent, startId);
 		motionTimer = new Timer(true);	// Create Timer as deamon
 	}
 
+	// AccuracyChanged not interesting 
 	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		float thres = 0.5f; // Event values groesser thres werden als Bewegung gewertet.
+		float thres = 0.5f; // Event values groesser thres werden als Bewegung gewertet. TODO: Thres aus Settings lesen
 		
 		float axisX = event.values[0];
 		float axisY = event.values[1];
 		float axisZ = event.values[2];
 		
-		if(axisX > thres){
-			Log.d(TAG, "Sensor changed: X = " + axisX );
-		};
-		if(axisY > thres){
-			Log.d(TAG, "Sensor changed: Y = " + axisY );
-		};
-		if(axisZ > thres){
-			Log.d(TAG, "Sensor changed: Z = " + axisZ );
-		};
 		if(axisX > thres || axisY > thres || axisZ > thres){
+			
 			Log.d(TAG, "Sensor changed, restarting timer" );
-			motionTimer.cancel();	// Cancel Tasks
-			motionTimer.purge();	// Remove Tasks
+			motionTimer.cancel();			// Cancel Tasks
+			motionTimer.purge();			// Remove Tasks
+			motionTimer = new Timer(true);	// Create new Timer as deamon
 			sendStatus = new BroadcastMotionDetectionStatus();
 			Log.d(TAG, "BroadcastMotionDetectionStatus created");
 			motionTimer.scheduleAtFixedRate(sendStatus, 0, refreshTime);
+			
 		}
 	};
 }
