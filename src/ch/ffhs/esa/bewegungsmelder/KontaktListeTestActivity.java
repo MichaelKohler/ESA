0c38323d47a99b6d9056886bbd4c6ef1a4b80566
Mario <Mario@Mario-PC>
2013-12-26 10:57:14 +0100

    Klasse für Kontaktobjekt hinzugefügt

--- src/ch/ffhs/esa/bewegungsmelder/KontaktListeTestActivity.java
+++ src/ch/ffhs/esa/bewegungsmelder/KontaktListeTestActivity.java
@@ -89,9 +89,9 @@ protected void onListItemClick (ListView l, View v, int position, long id ) {
 	        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
 	                null, null, null, null);
 	        if (cur.getCount() > 0) {
-	            while (cur.moveToNext()) {
-	                  String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-	                  String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+	        while (cur.moveToNext()) {
+	              String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+	              String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 	                
 	                  if (Integer.parseInt(cur.getString(
 	                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
---
+++ src/ch/ffhs/esa/bewegungsmelder/KontaktObjekt.java
@@ -0,0 +1,21 @@
+package ch.ffhs.esa.bewegungsmelder;
+
+public class KontaktObjekt {
+
+	private String name;
+	private String telnr;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNo() {
+		return telnr;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.telnr = telnr;
+	}
+	
+}
