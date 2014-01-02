b83a160f042c4463f91c5796afaa8e6ce1b0b1c5
Mario <Mario@Mario-PC>
2014-01-02 10:41:38 +0100

    Kontakt.java Klasse upgedatet

--- src/ch/ffhs/esa/bewegungsmelder/Kontakt.java
+++ src/ch/ffhs/esa/bewegungsmelder/Kontakt.java
@@ -3,19 +3,37 @@ package ch.ffhs.esa.bewegungsmelder;
 public class Kontakt {
 	private String name;
 	private String phoneNo;
+	private String objektID;
 	
 	public String getName() {
 		return name;
 	}
+	
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
 	public String getPhoneNo() {
 		return phoneNo;
 	}
+	
+	
 	public void setPhoneNo(String phoneNo) {
 		this.phoneNo = phoneNo;
 	}
+	
+	
+	public String getObjektID() {
+		return objektID;
+	}
+	
+	
+	public void setObjektID(String objektID) {
+		this.objektID = objektID;
+	}
+	
 	
 	
 }
